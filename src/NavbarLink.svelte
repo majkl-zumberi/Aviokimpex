80018f7708b0c2e4335994b29fe0b0b607f72c51
majkl zumberi <majklzumberi00@gmail.com>
2021-02-07 18:10:01 +0100

    feat(component): :sparkles: add navbarLink
    used from navbar component

---
+++ src/NavbarLink.svelte
@@ -0,0 +1,14 @@
+<script>
+    import {link,location} from 'svelte-spa-router';
+    export let path,label,isMobile;
+    let desktopViewClass="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium";
+    let mobileViewClass="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium";
+</script>
+<a
+  use:link 
+  href={path}
+  class="{isMobile?mobileViewClass:desktopViewClass}"
+  class:bg-gray-900={path==$location}
+  >
+    {label}
+</a>
