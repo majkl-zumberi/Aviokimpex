a3d8e1ddc3c875ebf085406193a1debfb532760f
majkl zumberi <majklzumberi00@gmail.com>
2021-02-07 23:38:52 +0100

    feat(i18n): :sparkles: add localeSwitcher component for changing lang

---
+++ src/LocaleSwitcher.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  export let value = "it";
+  const dispatch = createEventDispatcher();
+  function switchLocale(lang) {
+    dispatch("locale-changed", lang);
+    dispatch("closeProfile");
+  }
+</script>
+
+<a
+  on:click={() => switchLocale("it")}
+  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+  role="menuitem">Italiano</a
+>
+<a
+  on:click={() => switchLocale("en")}
+  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+  role="menuitem">English</a
+>
