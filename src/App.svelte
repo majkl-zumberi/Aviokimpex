0ae1a4d56512a976362bf6f2881473e42197d235
majkl zumberi <majklzumberi00@gmail.com>
2021-02-07 23:41:15 +0100

    feat(i18n): :sparkles: setup i18n for app.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,25 @@
 <script>
-	 import Router, {link} from 'svelte-spa-router';
-	 import Home from './Home.svelte';
-	 import Navbar from './Navbar.svelte';
-	 import Footer from './Footer.svelte';
-	 import About from './About.svelte';
-	 import Brands from './Brands.svelte';
-	 import Contacts from './Contacts.svelte';
+  import { locale } from "svelte-i18n";
+  import Router from "svelte-spa-router";
+  import About from "./About.svelte";
+  import Brands from "./Brands.svelte";
+  import Contacts from "./Contacts.svelte";
+  import Footer from "./Footer.svelte";
+  import Home from "./Home.svelte";
+  import Navbar from "./Navbar.svelte";
+  import { setupI18n } from "./services/i18n";
+
+  locale.set("it");
+  setupI18n({ withLocale: "it" });
 </script>
 
-<Navbar/>
-<Router routes={{
-	'/':Home,
-	'/about':About,
-	'/brands':Brands,
-	'/contacts':Contacts,
-}} />
-<Footer/>
+<Navbar />
+<Router
+  routes={{
+    "/": Home,
+    "/about": About,
+    "/brands": Brands,
+    "/contacts": Contacts,
+  }}
+/>
+<Footer />
