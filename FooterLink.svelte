4752b69f28b77b117f8514d8aab36979c414864f
majkl zumberi <majklzumberi00@gmail.com>
2021-02-07 21:55:45 +0100

    chore(component): :sparkles: add footerLink component

---
+++ FooterLink.svelte
@@ -0,0 +1,8 @@
+<script>
+    import {link} from 'svelte-spa-router';
+    export let path,label;
+</script>
+<li>
+    <div role="link" tabindex="-1"><a use:link class="ul-hover-effect text-gray-700" href={path}>{label}</a>
+    </div>
+</li>
