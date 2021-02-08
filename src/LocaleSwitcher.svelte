857bb2e0fe1f1918f3ce167fc4f1f4436e1e39b0
majkl zumberi <majklzumberi00@gmail.com>
2021-02-08 19:24:30 +0100

    fix(css): :bug: fix css for localeSwitcher

--- src/LocaleSwitcher.svelte
+++ src/LocaleSwitcher.svelte
@@ -11,11 +11,17 @@
 
 <a
   on:click={() => switchLocale("it")}
-  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+  class="z-50 block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
   role="menuitem">Italiano</a
 >
 <a
   on:click={() => switchLocale("en")}
-  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+  class="z-50 block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
   role="menuitem">English</a
 >
+
+<style>
+  a {
+    z-index: 999 !important;
+  }
+</style>
