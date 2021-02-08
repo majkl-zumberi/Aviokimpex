bf8efcf9aecad6a890ec94a266c0938761b6b743
majkl zumberi <majklzumberi00@gmail.com>
2021-02-08 19:22:50 +0100

    feat(css): :lipstick: add styles from swiper in app.svelte

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss";
+import "swiper/components/scrollbar/scrollbar.scss";
+import "swiper/swiper.scss";
 import App from './App.svelte';
 
 const app = new App({
