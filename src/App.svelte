52bba4b1d57e3289ae92717622f5a5e6bda2f08c
majkl zumberi <majklzumberi00@gmail.com>
2021-02-11 22:56:23 +0100

    feat(firebase): :sparkles: add firebase configuration
    using dotenv variables

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,11 @@
 <script>
+  import firebase from "firebase/app";
+  import "firebase/auth";
+  import "firebase/firestore";
+  import "firebase/performance";
   import { locale } from "svelte-i18n";
   import Router from "svelte-spa-router";
+  import { FirebaseApp } from "sveltefire";
   import About from "./About.svelte";
   import Brands from "./Brands.svelte";
   import Contacts from "./Contacts.svelte";
@@ -11,17 +16,30 @@
 
   locale.set("it");
   setupI18n({ withLocale: "it" });
+
+  const firebaseConfig = {
+    apiKey: _vars.env.apiKey,
+    authDomain: _vars.env.authDomain,
+    projectId: _vars.env.projectId,
+    storageBucket: _vars.env.storageBucket,
+    messagingSenderId: _vars.env.messagingSenderId,
+    appId: _vars.env.appId,
+  };
+
+  firebase.initializeApp(firebaseConfig);
 </script>
 
 <div class=" overflow-hidden">
   <Navbar />
-  <Router
-    routes={{
-      "/": Home,
-      "/about": About,
-      "/brands": Brands,
-      "/contacts": Contacts,
-    }}
-  />
+  <FirebaseApp {firebase}>
+    <Router
+      routes={{
+        "/": Home,
+        "/about": About,
+        "/brands": Brands,
+        "/contacts": Contacts,
+      }}
+    />
+  </FirebaseApp>
   <Footer />
 </div>
