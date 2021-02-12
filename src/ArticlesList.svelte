e41dbcc76ecbd7da4acb06f88665beaac3b9c928
majkl zumberi <majklzumberi00@gmail.com>
2021-02-12 22:56:46 +0100

    chore(component): :sparkles: add ArticleList component
    this component handle parameters from url (id_category) and query string

---
+++ src/ArticlesList.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { location, querystring } from "svelte-spa-router";
+
+  export let params = {};
+</script>
+
+<p>The current page is: {$location}</p>
+<p>The querystring is: {$querystring}</p>
+<p>id categoria {params.id_category}</p>
