b1113a6bc204e837e93c7902d3adfd1d5afecd3a
majkl zumberi <majklzumberi00@gmail.com>
2021-02-14 18:01:47 +0100

    feat(component): :sparkles: add banner
    added new InfoBanner component

---
+++ src/InfoBanner.svelte
@@ -0,0 +1,33 @@
+<section class="text-gray-600 body-font">
+  <div class="container px-5 py-16 mx-auto flex items-center flex-wrap">
+    <img
+      class="block h-14 w-auto"
+      src="/images/AvioKimpexsrl.png"
+      alt="kimpex logo"
+    />
+    <div class="md:w-2/5 md:pl-6">
+      <p class="leading-relaxed text-base">
+        Inserisci il <span class="font-medium">codice</span> o
+        <span class="font-medium">il nome</span> del prodotto che desideri ricercare
+      </p>
+      <div class="flex md:mt-4 mt-6">
+        <input
+          type="text"
+          class="flex-shrink flex-grow flex-auto leading-normal w-px flex-1 border h-10 border-grey-light rounded rounded-r-none px-3 relative focus:border-blue focus:shadow"
+          placeholder="es: microphone m87"
+        />
+        <button
+          class="inline-flex text-white bg-green-400 border-0 py-2 px-4 focus:outline-none hover:bg-green-600 "
+          >Cerca</button
+        >
+      </div>
+    </div>
+    <div class="flex  flex-col md:w-1/3 text-center">
+      <p class="leading-relaxed text-base text-2xl ">bisogno di assistenza?</p>
+      <p class="leading-relaxed text-base ">
+        non hai trovato il prodotto che cercavi? Chiamaci!
+      </p>
+      <p class="leading-relaxed text-base text-2xl">+39 030 2421830</p>
+    </div>
+  </div>
+</section>
