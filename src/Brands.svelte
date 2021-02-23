959f5705796f7ddd497ccfab0f63b02316138129
thomas <thomas.rossi2000@gmail.com>
2021-02-23 14:49:58 +0100

    update brands with two new cards

--- src/Brands.svelte
+++ src/Brands.svelte
@@ -107,6 +107,26 @@
               </div>
             </div>
           </div>
+
+          <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+            <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80">
+              <div class="flex-grow">
+                <h2 class="text-gray-900 title-font font-medium">example</h2>
+                <p class="text-gray-500">example</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+            <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80">
+              <div class="flex-grow">
+                <h2 class="text-gray-900 title-font font-medium">example</h2>
+                <p class="text-gray-500">example</p>
+              </div>
+            </div>
+          </div>
       
     </div>
     </div>
