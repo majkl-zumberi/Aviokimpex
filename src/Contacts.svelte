b161319d89509cf1689201d406c491d62f8f7266
thomas <thomas.rossi2000@gmail.com>
2021-03-08 14:25:42 +0100

    new contactus form

--- src/Contacts.svelte
+++ src/Contacts.svelte
@@ -1,13 +1,19 @@
-<section class="text-gray-600 body-font relative">
 
-    <div class="absolute inset-0 bg-gray-300">
-      <iframe width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="map" scrolling="no" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d320.39210876336256!2d10.224536431952265!3d45.527783728327634!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4781760e995540f7%3A0x71296509d6f55da0!2sVia%20Raffaele%20da%20Brescia%2C%2025%2C%2025124%20Brescia%20BS!5e0!3m2!1sit!2sit!4v1614285489545!5m2!1sit!2sit" style="filter: grayscale(1) contrast(1) opacity(0.7);"></iframe>
-    </div> 
+<!-- GOOGLE MAPS API WITH FEEDBACK FORM -->
 
-    <div class="container px-5 py-24 mx-auto flex">
-      <div class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
-        <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Contattaci</h2>
-        <p class="leading-relaxed mb-5 text-gray-600">Example</p>
+  <section class="text-gray-600 body-font relative">
+
+    <div class="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+      <div class="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+        <iframe width="100%" height="100%" class="absolute inset-0" frameborder="0" title="map" marginheight="0" marginwidth="0" scrolling="no" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d320.39210876336256!2d10.224536431952265!3d45.527783728327634!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4781760e995540f7%3A0x71296509d6f55da0!2sVia%20Raffaele%20da%20Brescia%2C%2025%2C%2025124%20Brescia%20BS!5e0!3m2!1sit!2sit!4v1614285489545!5m2!1sit!2sit" style="filter: grayscale(1) contrast(1) opacity(0.7);"></iframe>
+      </div>
+      <div class="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+        <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
+        <p class="leading-relaxed mb-5 text-gray-600">Grazie per averci contattato.</p>
+        <div class="relative mb-4">
+          <label for="name" class="leading-7 text-sm text-gray-600">Nome</label>
+          <input type="text" id="name" name="name" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+        </div>
         <div class="relative mb-4">
           <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
           <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
@@ -17,8 +23,7 @@
           <textarea id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
         </div>
         <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Invia</button>
-        <p class="text-xs text-gray-500 mt-3">example text</p>
+        <p class="text-xs text-gray-500 mt-3">Ti contatteremo nel minor tempo possibile.</p>
       </div>
     </div>
-
   </section>
