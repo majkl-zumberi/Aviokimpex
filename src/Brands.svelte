407a9f5ebcd880c3aa3fcc2c1286e265ec3958fb
thomas <thomas.rossi2000@gmail.com>
2021-03-24 12:09:17 +0100

    aggiunta immagini con inserimento in brands

--- src/Brands.svelte
+++ src/Brands.svelte
@@ -9,70 +9,63 @@
 
         <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
           <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-            <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80">
+            <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="images/baf.jpg">
             <div class="flex-grow">
-              <h2 class="text-gray-900 title-font font-medium">example</h2>
-              <p class="text-gray-500">example</p>
+              <h2 class="text-gray-900 title-font font-medium">baf</h2>
             </div>
           </div>
         </div>
 
         <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
             <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80">
+              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="images/champion.jpg">
               <div class="flex-grow">
-                <h2 class="text-gray-900 title-font font-medium">example</h2>
-                <p class="text-gray-500">example</p>
+                <h2 class="text-gray-900 title-font font-medium">champion</h2>
               </div>
             </div>
           </div>
 
           <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
             <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80">
+              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="images/Concorde.jpg">
               <div class="flex-grow">
-                <h2 class="text-gray-900 title-font font-medium">example</h2>
-                <p class="text-gray-500">example</p>
+                <h2 class="text-gray-900 title-font font-medium">concorde</h2>
               </div>
             </div>
           </div>
 
           <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
             <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80">
+              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="images/continental.jpg">
               <div class="flex-grow">
-                <h2 class="text-gray-900 title-font font-medium">example</h2>
-                <p class="text-gray-500">example</p>
+                <h2 class="text-gray-900 title-font font-medium">continental</h2>
               </div>
             </div>
           </div>
 
           <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
             <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80">
+              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="images/general.jpg">
               <div class="flex-grow">
-                <h2 class="text-gray-900 title-font font-medium">example</h2>
-                <p class="text-gray-500">example</p>
+                <h2 class="text-gray-900 title-font font-medium">general</h2>
               </div>
             </div>
           </div>
 
           <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
             <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80">
+              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="images/gill.jpg">
               <div class="flex-grow">
-                <h2 class="text-gray-900 title-font font-medium">example</h2>
-                <p class="text-gray-500">example</p>
+                <h2 class="text-gray-900 title-font font-medium">gill</h2>
               </div>
             </div>
           </div>
 
           <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
             <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80">
+              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="images/goodyear.jpg">
               <div class="flex-grow">
-                <h2 class="text-gray-900 title-font font-medium">example</h2>
-                <p class="text-gray-500">example</p>
+                <h2 class="text-gray-900 title-font font-medium">goodyear</h2>
               </div>
             </div>
           </div>
@@ -80,50 +73,72 @@
         
           <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
             <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80">
+              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="images/lycoming.png">
               <div class="flex-grow">
-                <h2 class="text-gray-900 title-font font-medium">example</h2>
-                <p class="text-gray-500">example</p>
+                <h2 class="text-gray-900 title-font font-medium">lycoming</h2>
               </div>
             </div>
           </div>
 
           <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
             <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80">
+              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="images/michelin.jpg">
               <div class="flex-grow">
-                <h2 class="text-gray-900 title-font font-medium">example</h2>
-                <p class="text-gray-500">example</p>
+                <h2 class="text-gray-900 title-font font-medium">michelin</h2>
               </div>
             </div>
           </div>
           
           <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
             <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80">
+              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="images/parker.jpg">
               <div class="flex-grow">
-                <h2 class="text-gray-900 title-font font-medium">example</h2>
-                <p class="text-gray-500">example</p>
+                <h2 class="text-gray-900 title-font font-medium">parker</h2>
               </div>
             </div>
           </div>
 
           <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
             <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80">
+              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="images/piper.png">
               <div class="flex-grow">
-                <h2 class="text-gray-900 title-font font-medium">example</h2>
-                <p class="text-gray-500">example</p>
+                <h2 class="text-gray-900 title-font font-medium">piper</h2>
               </div>
             </div>
           </div>
 
           <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
             <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80">
+              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="images/rapco.jpg">
               <div class="flex-grow">
-                <h2 class="text-gray-900 title-font font-medium">example</h2>
-                <p class="text-gray-500">example</p>
+                <h2 class="text-gray-900 title-font font-medium">rapco</h2>
+              </div>
+            </div>
+          </div>
+
+          <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+            <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="images/shell.png">
+              <div class="flex-grow">
+                <h2 class="text-gray-900 title-font font-medium">aeroshell</h2>
+              </div>
+            </div>
+          </div>
+
+          <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+            <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="images/tempest.jpg" images >
+              <div class="flex-grow">
+                <h2 class="text-gray-900 title-font font-medium">tempest</h2>
+              </div>
+            </div>
+          </div>
+
+          <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+            <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+              <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="images/timken.png">
+              <div class="flex-grow">
+                <h2 class="text-gray-900 title-font font-medium">timken</h2>
               </div>
             </div>
           </div>
