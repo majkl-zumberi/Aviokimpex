395afd98b62e3dac0b4cfd9f201bc823394a2093
majkl zumberi <majklzumberi00@gmail.com>
2021-05-01 16:29:12 +0200

    fix(component): :sparkles: add HomeBanner component into Home

--- src/Home.svelte
+++ src/Home.svelte
@@ -1,5 +1,9 @@
 <script>
   import Slideshow from "./Slideshow.svelte";
+  import HomeBanner from "./components/HomeBanner.svelte";
+  import AboutFeature from "./components/About-feature.svelte";
 </script>
 
 <Slideshow />
+<AboutFeature />
+<HomeBanner />
