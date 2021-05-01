20262271c50ba3c560d280d68ab4167293d64f07
majkl zumberi <majklzumberi00@gmail.com>
2021-05-01 20:14:40 +0200

    fix(component): :lipstick: fix Contacts component form button style

--- src/Contacts.svelte
+++ src/Contacts.svelte
@@ -65,7 +65,7 @@
               })}</label>
             <textarea id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
           </div>
-          <button type="submit" class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">{$_("contacts.send", {
+          <button type="submit" class="text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">{$_("contacts.send", {
             default:
               "Invia",
             })}</button>
