832ac9d44aed2bc2eeb7ab17f646f2746ffc7c8d
majkl zumberi <majklzumberi00@gmail.com>
2021-05-09 19:03:59 +0200

    fix(component): :bug: fix localeSwticher btns component

--- src/LocaleSwitcher.svelte
+++ src/LocaleSwitcher.svelte
@@ -11,20 +11,22 @@
   }
 </script>
 
+
 <button
   on:click={() => switchLocale("it")}
-  class="z-50 block bg-gray-100 px-4 py-2 text-sm text-gray-700 hover:bg-gray-300"
-  role="menuitem"><It/></button>
+  class="bg-gray-100 hover:bg-grey text-grey-darkest font-bold py-2 px-4 rounded inline-flex items-center"
+  role="menuitem">
+  <It />
+  <span></span>
+</button>
 <button
   on:click={() => switchLocale("en")}
-  class="z-50 block bg-gray-100 px-4 py-2 text-sm text-gray-700 hover:bg-gray-300"
+  class="bg-gray-100 hover:bg-grey text-grey-darkest font-bold py-2 px-4 rounded inline-flex items-center"
   role="menuitem"><Eu/></button>
 
 <style>
   button {
     z-index: 999 !important;
-    width: 25px;
-    height: 25px;
   }
 </style>
 
